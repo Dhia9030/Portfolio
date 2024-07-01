6a3519b76b89fef381c69e3f7ef21dd38a6dc7bd
Dhia . <mohameddhia.medini@insat.ucar.tn>
2024-07-01 14:38:32 +0100

    final touches

--- script.js
+++ script.js
@@ -15,46 +15,7 @@ var typed2 = new Typed(".auto-type2", {
 
 
 
-window.addEventListener('load', () => {
-    const navHeight = document.querySelector('.header-section').offsetHeight;
-    const links = document.querySelectorAll('.a');
 
-    links.forEach(link => {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-
-            const targetId = this.getAttribute('href').substring(1);
-            const targetElement = document.getElementById(targetId);
-            
-            if (targetElement) {
-                const targetPosition = targetElement.offsetTop - navHeight;
-
-                window.scrollTo({
-                    top: targetPosition,
-                    behavior: 'smooth'
-                });
-            }
-        });
-    });
-    const loader = document.getElementById('loader');
-    const content = document.getElementById('content');
-
-    
-    setTimeout(() => {
-        loader.style.opacity = '0';
-
-       
-        setTimeout(() => {
-            loader.style.display = 'none';
-            
-            content.style.height= '100%';
-        }, 10); 
-    }, 2000); 
-});
-
-window.onbeforeunload = function () {
-    window.scrollTo(0, 0);
-  }
 
 
 
